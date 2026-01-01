414396df4282fbb939e51f8fa6d44dee6b59cbfb
nakamuraitsuki <ituki1911n@gmail.com>
2026-01-01 18:28:03 +0900

    refactor: 図形描画を切り分け

--- wasabi/src/main.rs
+++ wasabi/src/main.rs
@@ -90,8 +90,7 @@ impl<'a> Iterator for MemoryMapIterator<'a> {
             None
         } else {
             let e: &EfiMemoryDescriptor = unsafe {
-                &*(self.map.memory_map_buffer.as_ptr().add(self.ofs)
-                    as *const EfiMemoryDescriptor)
+                &*(self.map.memory_map_buffer.as_ptr().add(self.ofs) as *const EfiMemoryDescriptor)
             };
             self.ofs += self.map.descriptor_size;
             Some(e)
@@ -110,7 +109,7 @@ impl MemoryMapHolder {
         }
     }
     pub fn iter(&self) -> MemoryMapIterator {
-        MemoryMapIterator { map: self, ofs: 0}
+        MemoryMapIterator { map: self, ofs: 0 }
     }
 }
 
@@ -127,8 +126,7 @@ struct EfiBootServicesTable {
     ) -> EfiStatus,
     /// プロトコルを探す
     _reserved1: [u64; 21],
-    exit_boot_services:
-        extern "win64" fn(image_handle: EfiHandle, map_key: usize) -> EfiStatus,
+    exit_boot_services: extern "win64" fn(image_handle: EfiHandle, map_key: usize) -> EfiStatus,
 
     _reserved4: [u64; 10],
     locate_protocol: extern "win64" fn(
@@ -149,8 +147,7 @@ impl EfiBootServicesTable {
     }
 }
 const _: () = assert!(offset_of!(EfiBootServicesTable, get_memory_map) == 56);
-const _: () =
-    assert!(offset_of!(EfiBootServicesTable, exit_boot_services) == 232);
+const _: () = assert!(offset_of!(EfiBootServicesTable, exit_boot_services) == 232);
 const _: () = assert!(offset_of!(EfiBootServicesTable, locate_protocol) == 320);
 
 #[repr(C)]
@@ -239,73 +236,19 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     )
     .unwrap();
 
-    exit_from_efi_boot_services(
-        image_handle,
-        efi_system_table,
-        &mut memory_map,
-    );
+    exit_from_efi_boot_services(image_handle, efi_system_table, &mut memory_map);
     writeln!(w, "Hello, Non-UEFI world!").unwrap();
     loop {
         hlt()
     }
 }
 
-fn draw_test_pattern<T: Bitmap>(buf: &mut T) {
-    let w = 128;
-    let left = buf.width() - w - 1;
-    let colors = [0x000000, 0xff0000, 0x00ff00, 0x0000ff];
-    let h = 64;
-    for (i, c) in colors.iter().enumerate() {
-        let y = i as i64 * h;
-        fill_rect(buf, *c, left, y, h, h).expect("fill_rect failed");
-        fill_rect(buf, !*c, left + h, y, h, h).expect("fill_rect failed");
-    }
-    let points = [(0, 0), (0, w), (w, 0), (w, w)];
-    for (x0, y0) in points.iter() {
-        for (x1, y1) in points.iter() {
-            let _ = draw_line(buf, 0xffffff, left + *x0, *y0, left + *x1, *y1);
-        }
-    }
-    draw_str_fg(buf, left, h * colors.len() as i64, 0x00ff00, "0123456789");
-    draw_str_fg(buf, left, h * colors.len() as i64 + 16, 0x00ff00, "ABCDEF");
-}
+
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {
         hlt()
-    }
-}
-
-trait Bitmap {
-    fn bytes_per_pixel(&self) -> i64;
-    fn pixels_per_line(&self) -> i64;
-    fn width(&self) -> i64;
-    fn height(&self) -> i64;
-    fn buf_mut(&mut self) -> *mut u8;
-    /// # Safety
-    /// 
-    /// Returned pointer is valid as log as the given coordinates are valid
-    /// which means that passing_is_in_*_range tests.
-    unsafe fn unchecked_pixel_at_mut(&mut self, x: i64, y: i64) -> *mut u32 {
-        self.buf_mut().add(
-            ((y * self.pixels_per_line() + x) * self.bytes_per_pixel())
-                as usize,
-        ) as *mut u32
-    }
-    fn pixel_at_mut(&mut self, x: i64, y: i64) -> Option<&mut u32> {
-        if self.is_in_x_range(x) && self.is_in_y_range(y) {
-            // SAFETY: (x, y) is always validated by the checks above.
-            unsafe { Some(&mut *(self.unchecked_pixel_at_mut(x, y)))}
-        } else {
-            None
-        }
-    }
-    fn is_in_x_range(&self, px: i64) -> bool {
-        0 <= px && px < min(self.width(), self.pixels_per_line())
-    }
-    fn is_in_y_range(&self, py: i64) -> bool {
-        0 <= py && py < self.height()
     }
 }
 
@@ -345,145 +288,7 @@ fn init_vram(efi_system_table: &EfiSystemTable) -> Result<VramBufferInfo> {
     })
 }
 
-/// # Safety
-/// 
-/// (x, y) must be a valid point in the buf.
-unsafe fn unchecked_draw_point<T: Bitmap>(
-    buf: &mut T,
-    color: u32,
-    x: i64,
-    y: i64,
-) {
-    *buf.unchecked_pixel_at_mut(x, y) = color;
-}
 
-fn draw_point<T: Bitmap>(
-    buf: &mut T,
-    color: u32,
-    x: i64,
-    y: i64,
-) -> Result<()> {
-    *(buf.pixel_at_mut(x, y).ok_or("Out of Range")?) = color;
-    Ok(())
-}
-
-fn fill_rect<T: Bitmap>(
-    buf: &mut T,
-    color: u32,
-    px: i64,
-    py: i64,
-    w: i64,
-    h: i64,
-) -> Result<()> {
-    if !buf.is_in_x_range(px)
-        || !buf.is_in_y_range(py)
-        || !buf.is_in_x_range(px + w - 1)
-        || !buf.is_in_y_range(py + h - 1)
-    {
-        return Err("Out of Range");
-    }
-    for y in py..py + h {
-        for x in px..px + w {
-            unsafe {
-                unchecked_draw_point(buf, color, x, y);
-            }
-        }
-    }
-    Ok(())
-}
-
-fn calc_slope_point(da: i64, db: i64, ia: i64) -> Option<i64> {
-    if da < db {
-        None
-    } else if da == 0 {
-        Some(0)
-    } else if (0..=da).contains(&ia) {
-        Some((2 * db * ia + da) / da / 2)
-    } else {
-        None
-    }
-}
-
-fn draw_line<T: Bitmap>(
-    buf: &mut T,
-    color: u32,
-    x0: i64,
-    y0: i64,
-    x1: i64,
-    y1: i64,    
-) -> Result<()> {
-    if !buf.is_in_x_range(x0)
-        || !buf.is_in_x_range(x1)
-        || !buf.is_in_y_range(y0)
-        || !buf.is_in_y_range(y1)
-    {
-        return Err("Out of Range");
-    }
-    let dx = (x1 - x0).abs();
-    let sx = (x1 - x0).signum();
-    let dy = (y1 - y0).abs();
-    let sy = (y1 - y0).signum();
-    if dx >= dy {
-        for (rx, ry) in (0..dx)
-            .flat_map(|rx| calc_slope_point(dx, dy, rx).map(|ry| (rx, ry)))
-        {
-            draw_point(buf, color, x0 + rx * sx, y0 + ry * sy)?;
-        }
-    } else {
-        for (rx, ry) in (0..dy)
-            .flat_map(|ry| calc_slope_point(dy, dx, ry).map(|rx| (rx, ry)))
-        {
-            draw_point(buf, color, x0 + rx * sx, y0 + ry * sy)?;
-        }
-    }
-    Ok(())
-}
-
-fn lookup_font(c: char) -> Option<[[char; 8]; 16]> {
-    const FONT_SOURCE: &str = include_str!("./font.txt");
-    if let Ok(c) = u8::try_from(c) {
-        let mut fi = FONT_SOURCE.split('\n');
-        while let Some(line) = fi.next() {
-            if let Some(line) = line.strip_prefix("0x") {
-                if let Ok(idx) = u8::from_str_radix(line, 16) {
-                    if idx != c {
-                        continue;
-                    }
-                    let mut font = [['*'; 8]; 16];
-                    for (y, line) in fi.clone().take(16).enumerate() {
-                        for (x, c) in line.chars().enumerate() {
-                            if let Some(e) = font[y].get_mut(x) {
-                                *e = c;
-                            }
-                        }
-                    }
-                    return Some(font);
-                }
-            }
-        }
-    }
-    None
-}
-
-fn draw_font_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, c: char) {
-    if let Some(font) = lookup_font(c){
-        for (dy, row) in font.iter().enumerate() {
-            for (dx, pixel) in row.iter().enumerate() {
-                let color = match pixel {
-                    '*' => color,
-                    _ => continue,
-                };
-                let _ = draw_point(buf, color, x + dx as i64, y + dy as i64);
-            }
-        }
-    }
-}
-
-fn draw_str_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, s: &str) {
-    for (i, c) in s.chars().enumerate() {
-        draw_font_fg(buf, x + i as i64 * 8, y, color, c);
-    }
-}
 
 struct VramTextWriter<'a> {
     vram: &'a mut VramBufferInfo,
@@ -492,7 +297,7 @@ struct VramTextWriter<'a> {
 }
 impl<'a> VramTextWriter<'a> {
     fn new(vram: &'a mut VramBufferInfo) -> Self {
-        Self { 
+        Self {
             vram,
             cursor_x: 0,
             cursor_y: 0,
@@ -522,10 +327,8 @@ fn exit_from_efi_boot_services(
     loop {
         let status = efi_system_table.boot_services.get_memory_map(memory_map);
         assert_eq!(status, EfiStatus::Success);
-        let status = (efi_system_table.boot_services.exit_boot_services)(
-            image_handle,
-            memory_map.map_key,
-        );
+        let status =
+            (efi_system_table.boot_services.exit_boot_services)(image_handle, memory_map.map_key);
         if status == EfiStatus::Success {
             break;
         }
