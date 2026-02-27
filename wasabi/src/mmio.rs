b39b0ec624978eaee582eab9cebb4403d64aaf04
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-27 18:33:42 +0900

    feat: Primary Event Ring と Command Ring

--- wasabi/src/mmio.rs
+++ wasabi/src/mmio.rs
@@ -1,8 +1,11 @@
 extern crate alloc;
 
 use alloc::boxed::Box;
-use core::mem::ManuallyDrop;
+use core::marker::PhantomPinned;
+use core::mem::{ManuallyDrop, MaybeUninit};
 use core::pin::Pin;
+
+use crate::x86::disable_cache;
 
 pub struct Mmio<T: Sized> {
     inner: ManuallyDrop<Pin<Box<T>>>,
@@ -28,4 +31,52 @@ impl<T> AsRef<T> for Mmio<T> {
     fn as_ref(&self) -> &T {
         self.inner.as_ref().get_ref()
     }
+}
+
+#[repr(align(4096))]
+pub struct IoBoxInner<T: Sized> {
+    data: T,
+    _pinned: PhantomPinned,
+}
+impl<T: Sized> IoBoxInner<T> {
+    pub fn new(data: T) -> Self {
+        Self {
+            data,
+            _pinned: PhantomPinned,
+        }
+    }
+}
+
+pub struct IoBox<T: Sized> {
+    inner: Pin<Box<IoBoxInner<T>>>,
+}
+impl<T: Sized> IoBox<T> {
+    pub fn new() -> Self {
+        let inner = Box::pin(IoBoxInner::new(unsafe {
+            MaybeUninit::<T>::zeroed().assume_init()
+        }));
+        let this = Self { inner };
+        disable_cache(&this);
+        this
+    }
+    /// #Safety
+    /// Same rules as Pin::get_unchecked_mut() applies.
+    pub unsafe fn get_unchecked_mut(&mut self) -> &mut T {
+        &mut self.inner.as_mut().get_unchecked_mut().data
+    }
+}
+impl<T> AsRef<T> for IoBox<T> {
+    fn as_ref(&self) -> &T {
+        &self.inner.as_ref().get_ref().data
+    }
+}
+impl<T: Sized> Default for IoBox<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[test_case]
+fn io_box_new() {
+    IoBox::<u64>::new();
 }
